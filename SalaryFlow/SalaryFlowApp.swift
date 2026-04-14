68dbc4e01dc1843bf8109067c5f5ad8dbd065d9d
ositoqian9865 <ositoqian9865@gmail.com>
2026-04-15 01:20:16 +0800

    Initial Commit

---
+++ SalaryFlow/SalaryFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryFlowApp.swift
+//  SalaryFlow
+//
+//  Created by 许倩 on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct SalaryFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SalaryFlowTests/SalaryFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryFlowTests.swift
+//  SalaryFlowTests
+//
+//  Created by 许倩 on 2026/4/15.
+//
+
+import Testing
+@testable import SalaryFlow
+
+struct SalaryFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
